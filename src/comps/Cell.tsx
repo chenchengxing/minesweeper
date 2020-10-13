d7ff331ab304903944f65555ec39be84f4134ecb
Jack Chen <ccxsungodzzx@gmail.com>
2020-10-13 19:56:52 +1100

    style: better style for cell in different status

--- src/comps/Cell.tsx
+++ src/comps/Cell.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Cell } from '../types/Cell'
+import { Cell, CellStatus } from '../types/Cell'
+import cn from 'classnames'
+import { getCellDisplayContent } from '../utils/get-cell-display-content'
 
 interface Props {
   cell: Cell,
@@ -9,9 +11,17 @@ function CellComp(props: Props) {
   const {
     cell,
   } = props
+  const { status } = cell
   return (
-    <div className={'cell'}>
-      { cell.numOfMinesAround }
+    <div 
+      className={
+        cn('cell', {
+          'cell-uncovered': status === CellStatus.Uncovered,
+          'cell-flagged': status === CellStatus.Flagged,
+        })
+      }
+    >
+      { getCellDisplayContent(cell) }
     </div>
   )
 }
