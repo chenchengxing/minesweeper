c45fcec9d65c44c7e1f8d7a57ba7de45ec50434c
Jack Chen <ccxsungodzzx@gmail.com>
2020-10-15 19:00:16 +1100

    chore: fix wrong test text

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders minesweeper title', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Minesweeper/i);
   expect(linkElement).toBeInTheDocument();
