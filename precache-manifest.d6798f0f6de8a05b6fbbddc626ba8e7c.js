22d9b1a9c3f5f44645fca13f6769946f271de352
Jack Chen <ccxsungodzzx@gmail.com>
2020-10-28 09:27:14 +1100

    Updates

---
+++ precache-manifest.d6798f0f6de8a05b6fbbddc626ba8e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b020e9dd2edc281af98834ba6d41a8a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751902ef38dcef47dceb",
+    "url": "/static/css/main.e35afaee.chunk.css"
+  },
+  {
+    "revision": "4839db1555a8b241f488",
+    "url": "/static/js/2.d7b7c954.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d7b7c954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751902ef38dcef47dceb",
+    "url": "/static/js/main.26d37d5a.chunk.js"
+  },
+  {
+    "revision": "5bf75b294f1ce371b4bc",
+    "url": "/static/js/runtime-main.c056d1e7.js"
+  }
+]);
